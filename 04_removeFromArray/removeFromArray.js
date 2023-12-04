1fa63191b62c3fcafe4db939c83c6fcb9925b549
Alfie <34726358+ToMuchTNT@users.noreply.github.com>
2023-12-04 23:19:03 +0000

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numbers, ...toRemove) {
+    let remove = [];
+    for (const num of toRemove) {
+        remove.push(num)        
+    }
+    console.log(numbers)
+    console.log(remove)
+    return numbers.filter(item => !remove.includes(item))
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
