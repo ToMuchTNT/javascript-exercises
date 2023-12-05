b240ab3edc58921223389bcb2b45fe505ffecddd
Alfie <34726358+ToMuchTNT@users.noreply.github.com>
2023-12-05 00:19:54 +0000

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    if (start >= end){
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    if (!(typeof start === 'number' && start >= 0)) {
+        return 'ERROR'
+    }
+    if (!(typeof end === 'number' && end >= 0)) {
+        return 'ERROR'
+    }
+    totalSum = 0
+    for (i=start;i<end+1;i++) {
+        totalSum += i
+    }
+    return totalSum
 };
 
 // Do not edit below this line
